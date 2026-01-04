871128f38ef7da6480d0d4bc0f80aea43bdc7086
gourharisaha222 <42261629+gourharisaha222@users.noreply.github.com>
2026-01-04 16:13:29 +0530

    Create docker_build.groovy

---
+++ vars/docker_build.groovy
@@ -0,0 +1,5 @@
+def call(String DockerHubUser, String ImageName, String ImageTag, String ProjFldr){
+  echo "This is Building the code"
+  sh "whoami"
+  sh "docker build -t ${DockerHubUser}/${ImageName}:${ImageTag} ./${ProjFldr}"
+}
